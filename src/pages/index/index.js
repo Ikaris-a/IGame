fb339bbfc9930c4030a2fc39d714bde9cdd87294
Iwaity <iwaity@IwaitydeMacBook-Pro.local>
2019-03-12 23:39:04 +0800

    feat home page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,58 +1,101 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
-import { observer, inject } from '@tarojs/mobx'
+import Taro, { Component } from "@tarojs/taro";
+import { View, Swiper, SwiperItem, Image } from "@tarojs/components";
+import { observer, inject } from "@tarojs/mobx";
+import { AtNavBar, AtTabBar } from "taro-ui";
+import "./index.less";
 
-import './index.less'
-
-
-@inject('counterStore')
+@inject("counterStore")
 @observer
 class Index extends Component {
-
   config = {
-    navigationBarTitleText: '首页'
+    navigationBarTitleText: "首页"
+  };
+
+  componentWillMount() {}
+
+  componentWillReact() {
+    console.log("componentWillReact");
   }
 
-  componentWillMount () { }
+  componentDidMount() {}
 
-  componentWillReact () {
-    console.log('componentWillReact')
-  }
+  componentWillUnmount() {}
 
-  componentDidMount () { }
+  componentDidShow() {}
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  componentDidHide() {}
 
   increment = () => {
-    const { counterStore } = this.props
-    counterStore.increment()
-  }
+    const { counterStore } = this.props;
+    counterStore.increment();
+  };
 
   decrement = () => {
-    const { counterStore } = this.props
-    counterStore.decrement()
-  }
+    const { counterStore } = this.props;
+    counterStore.decrement();
+  };
 
   incrementAsync = () => {
-    const { counterStore } = this.props
-    counterStore.incrementAsync()
-  }
-
-  render () {
-    const { counterStore: { counter } } = this.props
+    const { counterStore } = this.props;
+    counterStore.incrementAsync();
+  };
+  handleClick(value) {}
+  render() {
+    const {
+      counterStore: { counter }
+    } = this.props;
     return (
-      <View className='index'>
-        <Button onClick={this.increment}>+</Button>
-        <Button onClick={this.decrement}>-</Button>
-        <Button onClick={this.incrementAsync}>Add Async</Button>
-        <Text>{counter}</Text>
+      <View className="index">
+        <AtNavBar
+          onClickRgIconNd={this.handleClick}
+          onClickLeftIcon={this.handleClick}
+          color="#000"
+          title="首页 "
+          rightFirstIconType="bullet-list"
+        />
+
+        <Swiper
+          className="test-h"
+          indicatorColor="#999"
+          indicatorActiveColor="#1DA1F2"
+          indicatorDots
+          autoplay
+        >
+          <SwiperItem>
+            <Image
+              style="width:100%;background: #fff;"
+              src="http://img.dwstatic.com/lol/1512/314791918139/1450837187930.jpg"
+            />
+          </SwiperItem>
+          <SwiperItem>
+            <Image
+              style="width:100%;background: #fff;"
+              src="http://img3.dwstatic.com/lol/1512/314791918139/1450837176952.jpg"
+            />
+          </SwiperItem>
+          <SwiperItem>
+            <Image
+              style="width:100%;background: #fff;"
+              src="http://img3.dwstatic.com/lol/1512/315414597848/1451460779102.jpg"
+            />
+          </SwiperItem>
+        </Swiper>
+        <AtTabBar
+          fixed
+          tabList={[
+            {
+              title: "首页",
+              iconType: "home"
+            },
+            { title: "上传", iconType: "add" },
+            { title: "个人中心", iconType: "user", text: "100", max: "99" }
+          ]}
+          onClick={this.handleClick.bind(this)}
+          current={this.state.current}
+        />
       </View>
-    )
+    );
   }
 }
 
-export default Index 
+export default Index;
