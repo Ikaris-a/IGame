fd72da5862129e9676e005c7974162e522a15a9c
Iwaity <iwaity@IwaitydeMacBook-Pro.local>
2019-03-13 10:12:51 +0800

    feat add search

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,12 +1,19 @@
 import Taro, { Component } from "@tarojs/taro";
 import { View, Swiper, SwiperItem, Image } from "@tarojs/components";
 import { observer, inject } from "@tarojs/mobx";
-import { AtNavBar, AtTabBar } from "taro-ui";
+import { AtNavBar, AtTabBar, AtSearchBar, AtTabs, AtTabsPane } from "taro-ui";
 import "./index.less";
 
 @inject("counterStore")
 @observer
 class Index extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      value: "",
+      tabCurrent: 0
+    };
+  }
   config = {
     navigationBarTitleText: "首页"
   };
@@ -39,11 +46,22 @@ class Index extends Component {
     const { counterStore } = this.props;
     counterStore.incrementAsync();
   };
+  onChange(value) {
+    this.setState({
+      value: value
+    });
+  }
   handleClick(value) {}
+  handleTabClick(value) {
+    this.setState({
+      tabCurrent: value
+    });
+  }
   render() {
     const {
       counterStore: { counter }
     } = this.props;
+    const tabList = [{ title: "推荐" }, { title: "视频" }, { title: "图文" }];
     return (
       <View className="index">
         <AtNavBar
@@ -53,32 +71,58 @@ class Index extends Component {
           title="首页 "
           rightFirstIconType="bullet-list"
         />
-        <Swiper
-          className="test-h"
-          indicatorColor="#999"
-          indicatorActiveColor="#1DA1F2"
-          indicatorDots
-          autoplay
+        <AtSearchBar
+          value={this.state.value}
+          onChange={this.onChange.bind(this)}
+        />
+        <AtTabs
+          current={this.state.tabCurrent}
+          tabList={tabList}
+          onClick={this.handleTabClick.bind(this)}
         >
-          <SwiperItem>
-            <Image
-              style="width:100%;background: #fff;"
-              src="http://img.dwstatic.com/lol/1512/314791918139/1450837187930.jpg"
-            />
-          </SwiperItem>
-          <SwiperItem>
-            <Image
-              style="width:100%;background: #fff;"
-              src="http://img3.dwstatic.com/lol/1512/314791918139/1450837176952.jpg"
-            />
-          </SwiperItem>
-          <SwiperItem>
-            <Image
-              style="width:100%;background: #fff;"
-              src="http://img3.dwstatic.com/lol/1512/315414597848/1451460779102.jpg"
-            />
-          </SwiperItem>
-        </Swiper>
+          <AtTabsPane current={this.state.tabCurrent} index={0}>
+            <View style="padding: 100px 50px;background-color: #FAFBFC;text-align: center;">
+              推荐
+            </View>
+          </AtTabsPane>
+          <AtTabsPane current={this.state.tabCurrent} index={1}>
+            <View style="padding: 100px 50px;background-color: #FAFBFC;text-align: center;">
+              {" "}
+              <Swiper
+                className="test-h"
+                indicatorColor="#999"
+                indicatorActiveColor="#1DA1F2"
+                indicatorDots
+                autoplay
+              >
+                <SwiperItem>
+                  <Image
+                    style="width:100%;background: #fff;"
+                    src="http://img.dwstatic.com/lol/1512/314791918139/1450837187930.jpg"
+                  />
+                </SwiperItem>
+                <SwiperItem>
+                  <Image
+                    style="width:100%;background: #fff;"
+                    src="http://img3.dwstatic.com/lol/1512/314791918139/1450837176952.jpg"
+                  />
+                </SwiperItem>
+                <SwiperItem>
+                  <Image
+                    style="width:100%;background: #fff;"
+                    src="http://img3.dwstatic.com/lol/1512/315414597848/1451460779102.jpg"
+                  />
+                </SwiperItem>
+              </Swiper>
+            </View>
+          </AtTabsPane>
+          <AtTabsPane current={this.state.tabCurrent} index={2}>
+            <View style="padding: 100px 50px;background-color: #FAFBFC;text-align: center;">
+              图文
+            </View>
+          </AtTabsPane>
+        </AtTabs>
+
         <AtTabBar
           fixed
           tabList={[
